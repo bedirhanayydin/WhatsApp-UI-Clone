763696ae4657dc6b05887858312e165ad2caa2d7
bedirhhanaydin <bedirhhanaydin@gmail.com>
2022-03-21 20:23:08 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_clone/pages/chats_main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChatMainPage(),
+    );
+  }
+}
---
+++ lib/pages/chats_main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatMainPage extends StatelessWidget {
+  const ChatMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
